1aefc01d3c444c7dd2ac58c69ab8483ecb7df3ed
PavelStorin <storin@inbox.ru>
2026-06-20 00:59:51 +0300

    скролл popular-brands

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,6 +1,7 @@
 import Swiper from "swiper/bundle";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/scrollbar";
 
 export function initSlider() {
 	document.addEventListener("DOMContentLoaded", () => {
@@ -19,12 +20,33 @@ export function initSlider() {
 
 					1025: {
 						slidesOffsetAfter: 0,
+						scrollbar: {
+							dragSize: 80,
+						},
+					},
+
+					1280: {
+						scrollbar: {
+							dragSize: 100,
+						},
+					},
+
+					1440: {
+						scrollbar: {
+							dragSize: 120,
+						},
 					},
 				},
 
 				navigation: {
 					nextEl: ".swiper-button-next",
 					prevEl: ".swiper-button-prev",
+				},
+
+				scrollbar: {
+					el: ".swiper-scrollbar",
+					draggable: false,
+					dragSize: 120,
 				},
 
 				watchSlidesProgress: true,
